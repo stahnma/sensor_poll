26f4bb6869c315fb297aa67b6df18d1f680ade16
Michael Stahnke <stahnma@puppet.com>
2018-05-22 21:15:12 -0500

    Working, strawman

--- 1.go
+++ 1.go
@@ -3,19 +3,34 @@ package main
 import "github.com/morus12/dht22"
 import "fmt"
 import "os"
+import "time"
+import "log"
 
 func main() {
 	PIN := ""
 	if value, ok := os.LookupEnv("PIN"); ok {
 		PIN = "GPIO_" + value
 	} else {
-		PIN = "GPIO_17"
+		PIN = "GPIO_4"
 	}
 
 	sensor := dht22.New(PIN)
-	temperature, err := sensor.Temperature()
+	c1 := make(chan float32, 1)
+	go func() {
+		temperature, err := sensor.Temperature()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(temperature)
+		c1 <- temperature
+	}()
 
-	fmt.Println(temperature)
-	fmt.Println(err)
+	select {
+	case res := <-c1:
+		fmt.Println(c1)
+		fmt.Println(res)
+	case <-time.After(5 * time.Second):
+		fmt.Println("Operation timed out")
+	}
 
 }
